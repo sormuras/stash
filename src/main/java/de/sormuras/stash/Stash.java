3241265be375aeae74552aa059e89b7a04e7e19d
Christian Stein <sormuras@gmail.com>
2017-02-10 06:15:41 +0100

    Add properties to Stash and upgrade to google-java-format 1.3

--- src/main/java/de/sormuras/stash/Stash.java
+++ src/main/java/de/sormuras/stash/Stash.java
@@ -10,5 +10,12 @@ public interface Stash {
   /** Prevalent System Interface annotation. */
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
-  @interface Interface {}
+  @interface Interface {
+
+    /** @return super class of the generated stash class */
+    Class<?> classExtends() default Object.class;
+
+    /** @return {@code false} to omit generation of verification, defaults to {@code true}. */
+    boolean verify() default true;
+  }
 }
--- src/test/java/de/sormuras/stash/StashTests.java
+++ src/test/java/de/sormuras/stash/StashTests.java
@@ -1,13 +1,24 @@
 package de.sormuras.stash;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import readme.World;
 
 class StashTests {
 
   @Test
   void stashIsInterface() {
     assertTrue(Stash.class.isInterface());
+  }
+
+  @Test
+  void defaultInterfaceAnnotationValues() {
+    Stash.Interface annotation = World.class.getAnnotation(Stash.Interface.class);
+    assertNotNull(annotation);
+    assertSame(Object.class, annotation.classExtends());
+    assertTrue(annotation.verify());
   }
 }
